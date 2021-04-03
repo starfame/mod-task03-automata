72bd76b717857d8cc95ff9ad59fc0ade6a79e5ce
Igor Kozlov <79710144+starfame@users.noreply.github.com>
2021-04-04 00:49:20 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,13 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task, test1)
+{
+	Automata hot_drinks_machine = Automata();
+	hot_drinks_machine.on();
+	hot_drinks_machine.coin(100);
+	int result = hot_drinks_machine.choice(4);
+	EXPECT_EQ(0, result);
+}
+
+// остальные кейсы я рассмотрел в main
